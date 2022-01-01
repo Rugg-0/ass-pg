5507c7bd5b684dfb894cfa88cb82192d9e582e51
Rugg0 <Charlesattoh3@gmail.com>
2021-12-31 16:17:56 -0800

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,560 @@
+
+
+var face = [
+    {
+        trait: 'black',
+        weight: '1',
+        occurrence: '350 in 10000 editions (3.50 %)'
+    },
+    {
+        trait: 'clay',
+        weight: '1',
+        occurrence: '374 in 10000 editions (3.74 %)'
+    },
+    {
+        trait: 'darkbrown',
+        weight: '1',
+        occurrence: '324 in 10000 editions (3.24 %)'
+    },
+    {
+        trait: 'face_light_brown',
+        weight: '5',
+        occurrence: '1766 in 10000 editions (17.66 %)'
+    },
+    {
+        trait: 'peach',
+        weight: '5',
+        occurrence: '1846 in 10000 editions (18.46 %)'
+    },
+    {
+        trait: 'white',
+        weight: '5',
+        occurrence: '1732 in 10000 editions (17.32 %)'
+    },
+    {
+        trait: 'zombie',
+        weight: '10',
+        occurrence: '3608 in 10000 editions (36.08 %)'
+    }
+];
+
+
+var tee = [
+    {
+        trait: 'baby_pink',
+        weight: '10',
+        occurrence: '51 in 10000 editions (0.51 %)'
+    },
+    {
+        trait: 'black',
+        weight: '1',
+        occurrence: '4 in 10000 editions (0.04 %)'
+    },
+    {
+        trait: 'black_white',
+        weight: '10',
+        occurrence: '48 in 10000 editions (0.48 %)'
+    },
+    {
+        trait: 'blcyan',
+        weight: '10',
+        occurrence: '52 in 10000 editions (0.52 %)'
+    },
+    {
+        trait: 'bllue',
+        weight: '10',
+        occurrence: '47 in 10000 editions (0.47 %)'
+    },
+    {
+        trait: 'blue',
+        weight: '10',
+        occurrence: '44 in 10000 editions (0.44 %)'
+    },
+    {
+        trait: 'blues',
+        weight: '10',
+        occurrence: '39 in 10000 editions (0.39 %)'
+    },
+    {
+        trait: 'blue_pink_strip',
+        weight: '1',
+        occurrence: '6 in 10000 editions (0.06 %)'
+    },
+    {
+        trait: 'capsule',
+        weight: '10',
+        occurrence: '46 in 10000 editions (0.46 %)'
+    },
+    {
+        trait: 'green',
+        weight: '1',
+        occurrence: '2 in 10000 editions (0.02 %)'
+    },
+    {
+        trait: 'grey',
+        weight: '1',
+        occurrence: '5 in 10000 editions (0.05 %)'
+    },
+    {
+        trait: 'grey_black',
+        weight: '10',
+        occurrence: '42 in 10000 editions (0.42 %)'
+    },
+    {
+        trait: 'light_yellow',
+        weight: '1',
+        occurrence: '2 in 10000 editions (0.02 %)'
+    },
+    {
+        trait: 'pastel_2',
+        weight: '10',
+        occurrence: '37 in 10000 editions (0.37 %)'
+    },
+    {
+        trait: 'pastel_3',
+        weight: '10',
+        occurrence: '36 in 10000 editions (0.36 %)'
+    },
+    {
+        trait: 'pastel_4',
+        weight: '10',
+        occurrence: '49 in 10000 editions (0.49 %)'
+    },
+    {
+        trait: 'pastel_5',
+        weight: '10',
+        occurrence: '44 in 10000 editions (0.44 %)'
+    },
+    {
+        trait: 'pastel_6',
+        weight: '10',
+        occurrence: '38 in 10000 editions (0.38 %)'
+    },
+    {
+        trait: 'pastel_7',
+        weight: '10',
+        occurrence: '44 in 10000 editions (0.44 %)'
+    },
+    {
+        trait: 'pink',
+        weight: '10',
+        occurrence: '51 in 10000 editions (0.51 %)'
+    },
+    {
+        trait: 'pinky',
+        weight: '10',
+        occurrence: '35 in 10000 editions (0.35 %)'
+    }
+];
+
+var mouth = [
+    {
+        trait: 'mouth_brown',
+        weight: '1',
+        occurrence: '2000 in 10000 editions (20.00 %)'
+    },
+    {
+        trait: 'mouth_darkbrown',
+        weight: '1',
+        occurrence: '2067 in 10000 editions (20.67 %)'
+    },
+    {
+        trait: 'mouth_lightbrown',
+        weight: '1',
+        occurrence: '1992 in 10000 editions (19.92 %)'
+    },
+    {
+        trait: 'mouth_peach',
+        weight: '1',
+        occurrence: '1939 in 10000 editions (19.39 %)'
+    },
+    {
+        trait: 'mouth_red',
+        weight: '1',
+        occurrence: '2002 in 10000 editions (20.02 %)'
+    }
+];
+
+var eyes = [
+    {
+        trait: 'Black',
+        weight: '10',
+        occurrence: '4643 in 10000 editions (46.43 %)'
+    },
+    {
+        trait: 'Cyan',
+        weight: '5',
+        occurrence: '2206 in 10000 editions (22.06 %)'
+    },
+    {
+        trait: 'DarkBlue',
+        weight: '5',
+        occurrence: '2239 in 10000 editions (22.39 %)'
+    },
+    {
+        trait: 'LightBlue',
+        weight: '1',
+        occurrence: '461 in 10000 editions (4.61 %)'
+    },
+    {
+        trait: 'Violet',
+        weight: '1',
+        occurrence: '451 in 10000 editions (4.51 %)'
+    }
+];
+
+var hair = [
+    {
+        trait: 'AfroBlack',
+        weight: '10',
+        occurrence: '1033 in 10000 editions (10.33 %)'
+    },
+    {
+        trait: 'BlackTrim',
+        weight: '10',
+        occurrence: '997 in 10000 editions (9.97 %)'
+    },
+    {
+        trait: 'GreenLong',
+        weight: '5',
+        occurrence: '514 in 10000 editions (5.14 %)'
+    },
+    {
+        trait: 'LongCover',
+        weight: '1',
+        occurrence: '126 in 10000 editions (1.26 %)'
+    },
+    {
+        trait: 'LongHairBlack',
+        weight: '1',
+        occurrence: '105 in 10000 editions (1.05 %)'
+    },
+    {
+        trait: 'Mohawk',
+        weight: '1',
+        occurrence: '111 in 10000 editions (1.11 %)'
+    },
+    {
+        trait: 'Mohawk_Green',
+        weight: '1',
+        occurrence: '97 in 10000 editions (0.97 %)'
+    },
+    {
+        trait: 'Mohawk_Red',
+        weight: '1',
+        occurrence: '97 in 10000 editions (0.97 %)'
+    },
+    {
+        trait: 'NormalYellow',
+        weight: '5',
+        occurrence: '520 in 10000 editions (5.20 %)'
+    },
+    {
+        trait: 'PaleBrown',
+        weight: '10',
+        occurrence: '1050 in 10000 editions (10.50 %)'
+    },
+    {
+        trait: 'PinkLong',
+        weight: '5',
+        occurrence: '504 in 10000 editions (5.04 %)'
+    },
+    {
+        trait: 'SideCut',
+        weight: '2',
+        occurrence: '210 in 10000 editions (2.10 %)'
+    },
+    {
+        trait: 'Simple',
+        weight: '10',
+        occurrence: '1045 in 10000 editions (10.45 %)'
+    },
+    {
+        trait: 'Simple2',
+        weight: '10',
+        occurrence: '1098 in 10000 editions (10.98 %)'
+    },
+    {
+        trait: 'SimpleBlue',
+        weight: '5',
+        occurrence: '530 in 10000 editions (5.30 %)'
+    },
+    {
+        trait: 'SimpleBrown',
+        weight: '5',
+        occurrence: '544 in 10000 editions (5.44 %)'
+    },
+    {
+        trait: 'SimpleLightBlue',
+        weight: '5',
+        occurrence: '514 in 10000 editions (5.14 %)'
+    },
+    {
+        trait: 'SquareCut',
+        weight: '1',
+        occurrence: '86 in 10000 editions (0.86 %)'
+    },
+    {
+        trait: 'Violet',
+        weight: '3',
+        occurrence: '296 in 10000 editions (2.96 %)'
+    },
+    {
+        trait: 'Yellow',
+        weight: '5',
+        occurrence: '523 in 10000 editions (5.23 %)'
+    }
+];
+
+var earrings = [
+    {
+        trait: 'Gold',
+        weight: '10',
+        occurrence: '4388 in 10000 editions (43.88 %)'
+    },
+    {
+        trait: 'no',
+        weight: '1',
+        occurrence: '409 in 10000 editions (4.09 %)'
+    },
+    {
+        trait: 'OneSideBlack',
+        weight: '1',
+        occurrence: '426 in 10000 editions (4.26 %)'
+    },
+    {
+        trait: 'OneSideGold',
+        weight: '1',
+        occurrence: '384 in 10000 editions (3.84 %)'
+    },
+    {
+        trait: 'White',
+        weight: '10',
+        occurrence: '4393 in 10000 editions (43.93 %)'
+    }
+];
+
+var headgear = [
+    {
+        trait: 'black_mask',
+        weight: '1',
+        occurrence: '156 in 10000 editions (1.56 %)'
+    },
+    {
+        trait: 'cap',
+        weight: '1',
+        occurrence: '181 in 10000 editions (1.81 %)'
+    },
+    {
+        trait: 'capred',
+        weight: '2',
+        occurrence: '323 in 10000 editions (3.23 %)'
+    },
+    {
+        trait: 'cap_grey',
+        weight: '2',
+        occurrence: '322 in 10000 editions (3.22 %)'
+    },
+    {
+        trait: 'crown',
+        weight: '10',
+        occurrence: '1649 in 10000 editions (16.49 %)'
+    },
+    {
+        trait: 'cry',
+        weight: '10',
+        occurrence: '1642 in 10000 editions (16.42 %)'
+    },
+    {
+        trait: 'gold_glass',
+        weight: '10',
+        occurrence: '1620 in 10000 editions (16.20 %)'
+    },
+    {
+        trait: 'halo',
+        weight: '10',
+        occurrence: '1633 in 10000 editions (16.33 %)'
+    },
+    {
+        trait: 'hat',
+        weight: '1',
+        occurrence: '491 in 10000 editions (4.91 %)'
+    },
+    {
+        trait: 'hat2',
+        weight: '2',
+        occurrence: '491 in 10000 editions (4.91 %)'
+    },
+    {
+        trait: 'helmet_yellow',
+        weight: '1',
+        occurrence: '154 in 10000 editions (1.54 %)'
+    },
+    {
+        trait: 'mask',
+        weight: '10',
+        occurrence: '1665 in 10000 editions (16.65 %)'
+    },
+    {
+        trait: 'none',
+        weight: '1',
+        occurrence: '164 in 10000 editions (1.64 %)'
+    }
+];
+
+var eyewear = [
+    {
+        trait: 'Mask',
+        weight: '10',
+        occurrence: '3156 in 10000 editions (31.56 %)'
+    },
+    {
+        trait: 'no',
+        weight: '1',
+        occurrence: '323 in 10000 editions (3.23 %)'
+    },
+    {
+        trait: 'Pirate',
+        weight: '5',
+        occurrence: '1591 in 10000 editions (15.91 %)'
+    },
+    {
+        trait: 'Punks3D',
+        weight: '1',
+        occurrence: '308 in 10000 editions (3.08 %)'
+    },
+    {
+        trait: 'Stud',
+        weight: '5',
+        occurrence: '1572 in 10000 editions (15.72 %)'
+    },
+    {
+        trait: 'White',
+        weight: '10',
+        occurrence: '3050 in 10000 editions (30.50 %)'
+    }
+];
+
+var beard = [
+    {
+        trait: 'Brown',
+        weight: '10',
+        occurrence: '1166 in 10000 editions (11.66 %)'
+    },
+    {
+        trait: 'BrownPlain',
+        weight: '10',
+        occurrence: '1113 in 10000 editions (11.13 %)'
+    },
+    {
+        trait: 'Classy',
+        weight: '1',
+        occurrence: '121 in 10000 editions (1.21 %)'
+    },
+    {
+        trait: 'CleanShaved',
+        weight: '10',
+        occurrence: '1112 in 10000 editions (11.12 %)'
+    },
+    {
+        trait: 'Crew',
+        weight: '1',
+        occurrence: '107 in 10000 editions (1.07 %)'
+    },
+    {
+        trait: 'French',
+        weight: '5',
+        occurrence: '568 in 10000 editions (5.68 %)'
+    },
+    {
+        trait: 'Moustache',
+        weight: '1',
+        occurrence: '115 in 10000 editions (1.15 %)'
+    },
+    {
+        trait: 'MoustacheRevamp',
+        weight: '7',
+        occurrence: '786 in 10000 editions (7.86 %)'
+    },
+    {
+        trait: 'Plain',
+        weight: '10',
+        occurrence: '1162 in 10000 editions (11.62 %)'
+    },
+    {
+        trait: 'SantaClaus',
+        weight: '1',
+        occurrence: '115 in 10000 editions (1.15 %)'
+    },
+    {
+        trait: 'Sassy',
+        weight: '3',
+        occurrence: '339 in 10000 editions (3.39 %)'
+    },
+    {
+        trait: 'Simple',
+        weight: '10',
+        occurrence: '1126 in 10000 editions (11.26 %)'
+    },
+    {
+        trait: 'Small',
+        weight: '5',
+        occurrence: '589 in 10000 editions (5.89 %)'
+    },
+    {
+        trait: 'WhitePlain',
+        weight: '7',
+        occurrence: '763 in 10000 editions (7.63 %)'
+    },
+    {
+        trait: 'WhiteRevampMoustache',
+        weight: '7',
+        occurrence: '818 in 10000 editions (8.18 %)'
+    }
+];
+
+var extra = [
+    {
+        trait: 'Cig',
+        weight: '10',
+        occurrence: '3240 in 10000 editions (32.40 %)'
+    },
+    {
+        trait: 'Greens',
+        weight: '1',
+        occurrence: '317 in 10000 editions (3.17 %)'
+    },
+    {
+        trait: 'None',
+        weight: '20',
+        occurrence: '6443 in 10000 editions (64.43 %)'
+    }
+];
+
+
+function list(list, data, imgSrc){
+    var list = document.getElementById(`${list}`);
+    for(i = 0; i < data.length; i++){
+        var row = `<div class="box">
+                        <div class="img-box">
+                            <img src="./assets/img/${imgSrc}/${data[i].trait}.png" />
+                        </div>
+                        <h3>${data[i].trait}</h3>
+                        <h3>Weight ${data[i].weight}</h3>
+                        <h3>Occurrence: ${data[i].occurrence}</h3>
+                   </div>`
+        list.innerHTML += row
+    }
+}
+
+
+list("list1",face, "Face");
+list("list2", tee, "tee");
+list("list3", mouth, "mouth");
+list("list4", eyes, "eyes");
+list("list5", hair, "Hair");
+list("list6", earrings, "earrings");
+list("list7", headgear, "headgear");
+list("list8", eyewear, "eyewear");
+list("list9", beard, "beard");
+list("list10", extra, "extra");
